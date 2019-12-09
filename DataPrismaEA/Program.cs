d1f2eff5546614cda72ac9bf6773b2b3f06827d5
Aleksandar Ivanov <aleksandar.ivanov@observis.fi>
2019-12-09 10:12:29 +0200

    initial

---
+++ DataPrismaEA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MQTTnet;
+using MQTTnet.AspNetCore;
+
+namespace DataPrismaEA
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from alex.");
+            CreateWebHostBuilder(args).Build().Run();
+
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                    .UseKestrel(o =>
+                    {
+                        o.ListenAnyIP(1883, l => l.UseMqtt()); // mqtt pipeline
+                        o.ListenAnyIP(5000); // default http pipeline
+                    }).UseStartup<Startup>();
+        }
+    }
+}
---
+++ DataPrismaEA/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MQTTnet.AspNetCore;
+using MQTTnet.Diagnostics;
+using MQTTnet.Server;
+using MQTTnet.Adapter;
+using System.Net.WebSockets;
+
+namespace DataPrismaEA
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //this adds a hosted mqtt server to the services
+            //services.AddHostedMqttServer(builder => builder.WithDefaultEndpointPort(1883));
+
+            //this adds tcp server support based on Microsoft.AspNetCore.Connections.Abstractions
+            services.AddMqttConnectionHandler();
+
+            //this adds websocket support
+            services.AddMqttWebSocketServerAdapter();
+            ServiceCollectionExtensions.AddHostedMqttServer(services, new MqttServerOptions { });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            Console.WriteLine("Configuring backend.");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseMqttEndpoint();
+            app.UseWebSockets();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
